a63bf6e94566b355e29d97c343d3301618bb2a46
Nathan N <Nitharios@users.noreply.github.com>
2017-09-14 14:07:02 -1000

    Function names added, getHead and getTail method tests passing

--- linkedList.js
+++ linkedList.js
@@ -4,5 +4,44 @@
  * @return {Object} an object exposing methods to be used to manipulate a linked list
  */
 function linkedListGenerator(){
+  var head = null;
+  var tail = null;
+  var temp = {};
+  var current;
 
+  return {
+    getHead : getHead,
+    getTail: getTail,
+    add : add,
+    get : get,
+    remove : remove,
+    insert : insert
+  };
+
+  function getHead() {
+    return head;
+  }
+
+  function getTail() {
+    return tail;
+  }
+
+  function add(val) {
+    temp.next = {
+      value: val
+    };
+    return temp;
+  }
+
+  function get(num) {
+
+  }
+
+  function remove(num) {
+
+  }
+
+  function insert(val, num) {
+
+  }
 }
